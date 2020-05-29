2d0849d62daf52511892c76521f67209d5c6f2b0
Simone Santoni <simone.santoni.1@city.ac.uk>
2020-05-29 09:45:03 +0100

    Update _0.py

--- tutorials/_0/_0.py
+++ tutorials/_0/_0.py
@@ -53,8 +53,6 @@ import gensim
 from gensim import corpora
 from gensim.models import Phrases
 from gensim.corpora import Dictionary
-# plot
-import matplotlib.pyplot as plt
 
 
 # %% check software versions
@@ -122,43 +120,6 @@ docs = [article.strip().lower() for article in df.text]
 
 # hyphen to underscores
 docs = [re.sub(r'\b-\b', '_', text) for text in docs]
-
-
-# %% exploratory data analysis
-
-# barchart of the distribution of articles over time
-# --+ data series
-x = data.index
-y = time_slices
-# --+ labels
-x_labels = ['%s' % i for i in x if i < 2019] + ['2019*']
-y_labels = ['%s' % i for i in np.arange(0, 1400, 200)]
-# --+ create figure
-fig = plt.figure(figsize=(6, 4))
-# --+ populate figure with a plot
-ax = fig.add_subplot(1, 1, 1)
-# --+ plot data
-ax.bar(x, y, color='r', alpha=0.5)
-# --+ axis properties
-ax.set_xticks(x)
-ax.set_xticklabels(x_labels, fontsize=14, rotation='vertical')
-ax.set_xlabel('year', fontsize=14)
-ax.set_yticklabels(y_labels, fontsize=14)
-ax.set_ylabel('counts of documents', fontsize=14)
-# --+ annotation
-notes = """notes: * the 2019 bucket contains documents published
-              between Jan-01 and Mar-31."""
-plt.text(0.12, -0.25, notes, fontsize=12)
-# --+ grid
-ax.grid(True, ls='--', axis='y', color='white')
-# --+ save plot
-#plt.show()
-#folder = 'ss_1/exhibits'
-plt.savefig('articles_by_year.pdf',
-            transparent=True,
-            bbox_inches='tight',
-            pad_inches=0,
-            dpi=600)
 
 
 # %% NLP pipeline
