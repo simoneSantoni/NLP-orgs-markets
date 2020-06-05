4907f09e8856aecfdfc8e8c7a3e6fcd3b925253b
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-06-05 22:21:22 +0100

    Initial commit.

---
+++ tutorials/_1/_0.py
@@ -0,0 +1,18 @@
+#! /usr/env/bin python3
+
+"""
+Docstring
+------------------------------------------------------------------------------
+------------------------------------------------------------------------------
+
+Author: Simone Santoni, simone.santoni.1@city.ac.uk
+
+Edits: 
+       - created
+       - last change
+
+Notes:
+"""
+
+# %% load libraries
+import os:wq
