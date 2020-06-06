c9b0d9d8147bdc2ca99fb4eee3e0022a9e4f416f
simoneSantoni <simone.santoni.1@city.ac.uk>
2020-06-06 16:59:40 +0100

    Initial commit.

---
+++ tutorials/_3/_0.py
@@ -0,0 +1,32 @@
+# !/usr/bin/env python3
+
+"""
+Docstring
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+
+Author: Simone Santoni, simone.santoni.1@city.ac.uk
+
+Notes: Appreciating the approximate or phonetic distance between two string
+       is a common task when it comes to manipulate strings. The jellyfish
+       library for Python implements several distance metrics.
+
+       One of the metrics available in the jellyfish library is the 
+       Levenshtein Distance / Minimum Edit Distance. One possible application
+       is identifying succesfull 
+
+       [1] https://github.com/jamesturk/jellyfish
+
+"""
+
+mail = """
+
+Hi there-
+
+over the last few days, I've been working on task ABC. The below-displayed
+code solves the problem:
+
+> for i in np.arange(0, 10, 1):
+>    print(i)
+"""
